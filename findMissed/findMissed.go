b92849b5367fba4d071ef4b0c07e95cea79a3221
KolesnikovOleg <OKolesnikov1812@gmail.com>
2022-11-25 17:56:37 +0300

    переписал поиск отсутсвующего

--- findMissed/findMissed.go
+++ findMissed/findMissed.go
@@ -7,24 +7,18 @@ import (
 )
 
 func Solution(A []int) int {
-	hashMap := make(map[int]int)
-	for id, value := range A {
-		hashMap[value] = id
+	counter := len(A) + 1
+	sum := 0
+	for i := 0; i < len(A); i++ {
+		counter += i + 1
+		sum += A[i]
 	}
-	for i := 1; i < len(A)+2; i++ {
-		_, find := hashMap[i]
-		if !find {
-			return i
-		}
-	}
-
-	fmt.Println("something happened wrong!!!")
-	return -1
+	return counter - sum
 }
 
 func main() {
 	//numer fo array generation
-	N := 5
+	N := 10000
 	A := []int{}
 	rand.Seed(time.Now().UnixNano())
 	deleteId := rand.Intn(N) + 1
@@ -35,5 +29,5 @@ func main() {
 	}
 	rand.Shuffle(len(A), func(i, j int) { A[i], A[j] = A[j], A[i] })
 	fmt.Println("excepted value", deleteId)
-	fmt.Println("Solution", Solution(A), A)
+	fmt.Println("Solution", Solution(A))
 }
